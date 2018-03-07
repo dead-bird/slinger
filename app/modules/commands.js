acd4e7da4fb438f91edd812b97e18f8390ea822b
DaveP <dave@wearecrystal.uk>
2018-03-07 15:38:36 +0000

    Stats command

--- app/modules/commands.js
+++ app/modules/commands.js
@@ -1,7 +1,6 @@
 require('dotenv').config({path: '.env'});
 
 const Discord = require('discord.js'),
-      fs      = require('fs'),
       env     = process.env;
 
 module.exports = {
@@ -15,8 +14,19 @@ module.exports = {
   stats: {
     desc: 'Show off your glorious stats',
     args: '',
-    execute: () => {
-      console.log('stats should include rank, wins, losses, shots fired, hits, misses, K/D (needs calcing)');
+    execute: (client, msg, args, core) => { // K/D needs thinking about
+      let user  = core.getOutlaw(client, msg.author.id);
+      let embed = new Discord.RichEmbed()
+        .setColor(3447003)
+        .setDescription(`stats for <@${msg.author.id}>`)
+        .setThumbnail(client.user.avatarURL)
+        .addBlankField(true);
+
+      for (stat in user) {
+        if (stat !== 'id') embed.addField('\u200B', `**${stat}:** ${user[stat]}`);
+      }
+
+      msg.channel.send({embed});
     }
   },
 };
