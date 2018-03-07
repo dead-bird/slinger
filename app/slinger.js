2e3a188d7168276d206dd2f14bd3634172cd473a
DaveP <dave@wearecrystal.uk>
2018-03-07 00:06:54 +0000

    Started main file

---
+++ app/slinger.js
@@ -0,0 +1,25 @@
+require('dotenv').config({path: '.env'});
+
+const Discord       = require('discord.js'),
+      EnmapLevel    = require('enmap-level'),
+      core          = require('./modules/core.js'),
+      Enmap         = require('enmap'),
+      client        = new Discord.Client,
+      env           = process.env;
+      
+client.on('ready', () => {
+  client.config = new Enmap({provider: new EnmapLevel({name: 'config'})});
+  client.users  = new Enmap({provider: new EnmapLevel({name: 'users'})});
+
+  core.init(client);
+});
+
+client.on('message', msg => {
+  core.respond(msg);
+});
+
+client.on('guildCreate', guild => {
+  core.guild(client, guild.id);
+});
+
+client.login(env.TOKEN);
