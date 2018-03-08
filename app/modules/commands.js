7abe366eaaab4cd2b2f5cdd669160459b6eae014
DaveP <dave@wearecrystal.uk>
2018-03-08 17:42:12 +0000

    Help command

--- app/modules/commands.js
+++ app/modules/commands.js
@@ -5,14 +5,34 @@ const Discord = require('discord.js'),
 
 module.exports = {
   help: {
+    icon: ':bellhop:',
     desc: 'Lists my commands',
     args: '',
-    execute: () => {
-      console.log('commands');
+    execute: (client, msg) => {
+      msg.delete().then().catch(console.error);
+
+      let prefix = client.config.get(msg.guild.id).prefix;
+
+      let embed = new Discord.RichEmbed()
+        .setColor(3447003)
+        .setDescription(':information_source: here are my commands')
+        .setThumbnail(client.user.avatarURL) // Bot's avatar
+        .addBlankField(true);
+
+      for (var trigger in module.exports) {
+        if (trigger) {
+          let cmd = module.exports[trigger];
+
+          embed.addField('\u200B', `**${cmd.icon} ${prefix}${trigger} ${cmd.args}**\n:white_small_square: ${cmd.desc}`);
+        }
+      }
+
+      msg.channel.send({embed});
     }
   },
   stats: {
-    desc: 'Show off your glorious stats',
+    icon: ':muscle:',
+    desc: 'Flex your stats',
     args: '',
     execute: (client, msg, args, core) => { // K/D needs thinking about
       let user  = core.getOutlaw(client, msg.author.id);
@@ -29,4 +49,12 @@ module.exports = {
       msg.channel.send({embed});
     }
   },
+  duel: {
+    icon: ':gun:',
+    desc: 'Challenge another user to a gunfight',
+    args: '@user',
+    execute: () => {
+      
+    }
+  }
 };
