bd65abc08ce89a50be9375e0dadb6b4ada73283a
DaveP <dave@wearecrystal.uk>
2018-10-04 23:17:45 +0100

    Rebuild for Clapp

--- app/slinger.js
+++ app/slinger.js
@@ -1,25 +1,61 @@
-require('dotenv').config({path: '.env'});
+require('dotenv').config({ path: '.env' });
 
-const Discord       = require('discord.js'),
-      EnmapLevel    = require('enmap-level'),
-      core          = require('./modules/core.js'),
-      Enmap         = require('enmap'),
-      client        = new Discord.Client,
-      env           = process.env;
-      
-client.on('ready', () => {
-  client.config = new Enmap({provider: new EnmapLevel({name: 'config'})});
-  client.outlaws  = new Enmap({provider: new EnmapLevel({name: 'outlaws'})});
+const { Client } = require('discord.js');
+const EnmapLevel = require('enmap-level');
+const pkg = require('../package.json');
+const Clapp = require('clapp');
+const env = process.env;
+const bot = new Client();
+const fs = require('fs');
+// core = require('./modules/core.js'),
+// Enmap = require('enmap'),
 
-  core.init(client);
+const app = new Clapp.App({
+  name: 'Slinger',
+  desc: pkg.description,
+  prefix: 'slinger',
+  separator: ' ',
+  version: pkg.version,
+  onReply(msg, context) {
+    return context.msg.reply('\n' + 'howdy');
+  },
 });
 
-client.on('message', msg => {
-  core.respond(client, msg, core);
+fs.readdirSync('./app/commands/').forEach(file => {
+  app.addCommand(require('./commands/' + file));
 });
 
-client.on('guildCreate', guild => {
-  core.createGuild(client, guild.id);
+bot.on('message', msg => {
+  if (app.isCliSentence(msg.content)) {
+    app.parseInput(msg.content, { msg });
+  }
 });
 
-client.login(env.TOKEN);
+bot.on('ready', () => {
+  console.log('howdy');
+  // client.config = new Enmap({provider: new EnmapLevel({name: 'config'})});
+  // client.outlaws  = new Enmap({provider: new EnmapLevel({name: 'outlaws'})});
+  // bot.user.setActivity('💯', { type: 'Playing' });
+});
+
+// client.on('guildCreate', guild => {
+//   core.createGuild(client, guild.id);
+// });
+
+// createGuild: (client, id) => {
+//   client.config.set(id, {
+//     prefix: "!"
+//   });
+// },
+// getOutlaw: (client, id) => {
+//   return client.outlaws.get(id) || self.createOutlaw(client, id);
+// },
+// createOutlaw: (client, id) => {
+//   let outlaw = { id: id, rank: 0, wins: 0, losses: 0, shots: 0, hits: 0, misses: 0 }
+
+//   client.config.set(id, outlaw);
+
+//   return outlaw;
+// }
+
+bot.login(env.TOKEN).catch(console.error);
