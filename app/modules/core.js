acec277b21e6eefcb10befa6ae5b0cd7168f5888
DaveP <dave@wearecrystal.uk>
2018-10-07 00:13:19 +0100

    User Status helper function

--- app/modules/core.js
+++ app/modules/core.js
@@ -5,5 +5,8 @@ module.exports = {
   user: {
     // Grab ID from <!id> or <id>
     id: id => id.replace(/<@!(\d*)>|<@(\d*)>/g, '$1'),
+
+    // Get User from ID and return status
+    status: (guild, id) => guild.members.get(id).user.presence.status,
   },
 };
